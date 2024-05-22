231d4960870bb46aeac2c49de470c8ccd4b31d72
Arik Mozharovsky <arikmozh@gmail.com>
2024-05-22 17:01:07 +0300

    changed port

--- server/src/server.js
+++ server/src/server.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 const otpRoutes = require('./routes/otp');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 connectDB();
 
